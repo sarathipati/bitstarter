6b9239d7baa35f03431b5a5a3739e345d3edcdee
Ubuntu <ubuntu@ip-172-31-21-210.us-west-2.compute.internal>
2016-01-12 15:35:57 +0000

    Changes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
+var contents = fs.readFileSync('/bitstarter/index.html').toString()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('/bitstarter/index.html'. 'utf8'))
+  response.send(contents)
 })
 
 app.listen(app.get('port'), function() {
